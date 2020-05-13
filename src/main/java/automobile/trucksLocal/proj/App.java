81d6830ad7fe4bb7c27d26a8ea6ccc86e45ff0e4
Dev1 <nikhil.lobo@my.uwrf.edu>
2020-05-12 21:58:14 -0500

    Changes

---
+++ src/main/java/automobile/trucksLocal/proj/App.java
@@ -0,0 +1,13 @@
+package automobile.trucksLocal.proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
